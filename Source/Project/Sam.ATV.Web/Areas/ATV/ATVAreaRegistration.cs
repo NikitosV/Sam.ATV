c4f7761a90a5573113312857ec9967419abbe812
NikitosV <Niktos.V@tut.by>
2019-10-30 17:41:44 +0300

    add base WEB and Serialized projects

---
+++ Source/Project/Sam.ATV.Web/Areas/ATV/ATVAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sam.ATV.Web.Areas.ATV
+{
+    public class ATVAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ATV";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ATV_default",
+                "ATV/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Source/Project/Sam.ATV.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sam.ATV.Web.MvcApplication" Language="C#" %>
