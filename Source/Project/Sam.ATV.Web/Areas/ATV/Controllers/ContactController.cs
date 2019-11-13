6376c831bdb2fd34905e5ea53e0c1560cc249390
NikitosV <Niktos.V@tut.by>
2019-11-13 10:47:49 +0300

    some fix

---
+++ Source/Project/Sam.ATV.Web/Areas/ATV/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sam.ATV.Web.Areas.ATV.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: ATV/Contact
+        public ViewResult Index()
+        {
+            return View("~/Areas/ATV/Views/Contact/Index.cshtml");
+        }
+    }
+}
